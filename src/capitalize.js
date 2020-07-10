292f86ab4ef04f9db59aa248d9ae5f9ae1bb0f65
Alex <bangau.alexandru@gmail.com>
2020-07-10 11:48:55 +0200

    Remove files pushed by mistake

--- src/capitalize.js
+++
@@ -1,3 +0,0 @@
-exports.capitalize = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
--- src/reverseString.js
+++
@@ -1,3 +0,0 @@
-exports.reverseString = (string) => {
-  return string.split("").reverse().join("")
-}
